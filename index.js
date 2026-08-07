5284a08b29cd516c6e8b47b6a8b25a0d85e8ee02
BrahiamS7 <bfsoto16@gmail.com>
2026-08-07 13:32:34 -0500

    Configurando detalles finales

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 import usuariosRouter from "./routes/usuarios.routes.js";
 import postsRouter from "./routes/post.routes.js";
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
